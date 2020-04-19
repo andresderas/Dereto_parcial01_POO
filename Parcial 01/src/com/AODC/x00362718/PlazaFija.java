e1d77300d86f8df1f78adfef6f9ad7f5325b61ef
Andres Deras <derasandres@gmail.com>
2020-04-18 21:07:09 -0600

    Add: class PlazaFija

---
+++ Parcial 01/src/com/AODC/x00362718/PlazaFija.java
@@ -0,0 +1,13 @@
+package com.AODC.x00362718;
+
+public class PlazaFija extends Empleado{
+    //Atributo
+    private int extension;
+    //Constructor
+    public PlazaFija(String nombre, String puesto, double salario, int extension) {
+        super(nombre, puesto, salario);
+        this.extension = extension;
+    }
+    //Metodos
+
+}
