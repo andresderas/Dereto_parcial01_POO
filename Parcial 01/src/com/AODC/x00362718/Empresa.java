c704d691de517aefd0399110096dfe2724abf255
José Roberto Hernández <00366718@uca.edu.sv>
2020-04-18 20:30:59 -0600

    Add: class Empresa

---
+++ Parcial 01/src/com/AODC/x00362718/Empresa.java
@@ -0,0 +1,29 @@
+package com.AODC.x00362718;
+
+import java.util.ArrayList;
+
+public class Empresa {
+    private String nombre;
+    private ArrayList<Empleado> planilla;
+
+    //Constructor
+    public Empresa(String nombre) {
+        this.nombre = nombre;
+        planilla = new ArrayList<Empleado>();
+    }
+    //Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public ArrayList<Empleado> getPlanilla() {
+        return planilla;
+    }
+    //Metodos
+    public void addEmpleado(Empleado empleado){
+    }
+
+    public void quitEmpleado(String nom){
+
+    }
+}
