d9c1f3636ed400aa30dd6c971e9efd67d799c8f7
Andres Deras <derasandres@gmail.com>
2020-04-18 20:50:52 -0600

    Add: class CalculadoraImpuestos

---
+++ Parcial 01/src/com/AODC/x00362718/CalculadoraImpuestos.java
@@ -0,0 +1,12 @@
+package com.AODC.x00362718;
+
+public final class CalculadoraImpuestos {
+    //Atributos
+    private static double totalRenta;
+    private static double totalISSS;
+    private static double totalAFP;
+    //Constructor privado
+    private CalculadoraImpuestos() {}
+    //Metodos estaticos
+
+}
